f2d53d13ab80ec2184721d88cd494ceae2e3a0c0
Sherko <xxxtutorial6@gmail.com>
2025-11-02 05:39:09 +0100

    Configuration finale de la base de données pour Render

--- PhishEye/settings.py
+++ PhishEye/settings.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 import os # Assure-toi que os est importé
 from decouple import config
-
+import dj_database_url
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -45,6 +45,14 @@ DATABASES = {
         'PORT': '5432',
     }
 }
+
+if 'DATABASE_URL' in os.environ:
+    DATABASES['default'] = dj_database_url.config(
+        conn_max_age=600,
+        conn_health_checks=True,
+        ssl_require=True,
+    )
+
 
 TEMPLATES = [
     {
